d85128965e52120dcd027454d31c290ae7e70b56
KishoreAnanth18 <sritharani.cs20@bitsathy.ac.in>
2024-06-08 16:05:18 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import SpiralModel from './components/SpiralModel';
+import WaveModel from './components/WaveModel';
 
-function App() {
+const App = () => {
+  const [selectedModel, setSelectedModel] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Parkinson's Disease Prediction</h1>
+      <div className='button-container'>
+      <div className='model-button' onClick={() => setSelectedModel('spiral')}><button class="button-64"><span class="text">Spiral Model</span></button></div>
+      <div className='model-button' onClick={() => setSelectedModel('wave')}><button class="button-64"><span class="text">Wave Model</span></button></div>
+      </div>
+      <div>
+      {selectedModel === 'spiral' && <SpiralModel />}
+      {selectedModel === 'wave' && <WaveModel />}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
